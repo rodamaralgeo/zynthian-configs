e04378f6f1ed883939e5485e9d6c800c79bd3bff
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-04-30 16:04:39 -0300

    Update and rename install_mod_pitchshifter.sh to install_mod-pitchshifter.sh

--- install_mod-pitchshifter.sh
+++ install_mod-pitchshifter.sh
@@ -2,7 +2,8 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/moddevices/mod-pitchshifter.git
 cd mod-pitchshifter
-sed -i -- 's/INSTALLATION_PATH = \$(DESTDIR)\$(INSTALL_PATH)\/\$(EFFECT_PATH)/INSTALLATION_PATH = \/zynthian\/zynthian-plugins\/lv2\/mod-pitchshifter/' Makefile.mk
+sed -i -- 's,-mtune=generic -msse -msse2 -mfpmath=sse,,g' Makefile.mk
+sed -i -- 's,INSTALL_PATH = /usr/local/lib/lv2,INSTALL_PATH = ${ZYNTHIAN_PLUGINS_DIR}/lv2,g' Makefile.mk
 make -j 4 NOOPT=true
 sudo make install
 make clean
