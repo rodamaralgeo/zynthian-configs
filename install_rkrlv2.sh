3eeb2e5e135fab3ce6d291d366fb747128aea0fe
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-04-30 20:20:01 -0300

    Update install_rkrlv2.sh

--- install_rkrlv2.sh
+++ install_rkrlv2.sh
@@ -1,1 +1,13 @@
-
+# rkrlv2
+cd $ZYNTHIAN_PLUGINS_SRC_DIR
+git clone https://github.com/ssj71/rkrlv2.git
+cd rkrlv2
+sed -i -- 's,-msse -msse2 -mfpmath=sse,,g' lv2/CMakeLists.txt
+sed -i -- 's,LV2_INSTALL_DIR lib/lv2/rkr.lv2,LV2_INSTALL_DIR ${ZYNTHIAN_PLUGINS_DIR}/lv2/rkr.lv2,g' lv2/CMakeLists.txt
+mkdir build
+cd build
+cmake ..
+make MOD=1
+make install MOD=1
+make clean
+cd ../..
