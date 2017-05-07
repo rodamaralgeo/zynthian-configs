866073fc714c51220da494153682c16bb665d85d
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-05-07 17:30:43 -0300

    Update install_rkrlv2.sh

--- install_rkrlv2.sh
+++ install_rkrlv2.sh
@@ -3,7 +3,7 @@ cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/ssj71/rkrlv2.git
 cd rkrlv2
 sed -i -- 's,-msse -msse2 -mfpmath=sse,,g' lv2/CMakeLists.txt
-sed -i -- 's,LV2_INSTALL_DIR lib/lv2/rkr.lv2,LV2_INSTALL_DIR ${ZYNTHIAN_PLUGINS_DIR}/lv2/rkr.lv2,g' lv2/CMakeLists.txt
+sed -i -- 's,LV2_INSTALL_DIR lib/lv2/rkr.lv2,LV2_INSTALL_DIR /zynthian/zynthian-plugins/lv2/rkr.lv2,g' lv2/CMakeLists.txt
 mkdir build
 cd build
 cmake ..
