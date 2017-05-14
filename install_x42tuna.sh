9d808ead9817e2dbb9c0e1ba713ce708d6d14871
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-05-14 00:37:53 -0300

    Update install_x42tuna.sh

--- install_x42tuna.sh
+++ install_x42tuna.sh
@@ -4,6 +4,7 @@ cd tuna.lv2
 sed -i -- 's,PREFIX ?= /usr/local,PREFIX ?= /zynthian/zynthian-plugins,g' Makefile
 sed -i -- 's,LV2DIR ?= $(PREFIX)/lib/lv2,LV2DIR ?= $(PREFIX)/lv2,g' Makefile 
 sed -i -- 's,-msse -msse2 -mfpmath=sse,,g' Makefile 
-make submodules
-make -j 4 MOD=1
-sudo make install MOD=1
+git submodule init
+git submodule update
+make MOD=1
+make install MOD=1
