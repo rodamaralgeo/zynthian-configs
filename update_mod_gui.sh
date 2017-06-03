49982e13ffc39ed061ab64bff86aae2600a6d71d
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-06-03 18:26:00 -0300

    Update update_mod_gui.sh

--- update_mod_gui.sh
+++ update_mod_gui.sh
@@ -1,60 +1,31 @@
-#Install_mod-lv2-data.sh
-cd $ZYNTHIAN_PLUGINS_SRC_DIR
-#git clone https://github.com/moddevices/mod-lv2-data.git
-
-#Apply mod-lv2-data
-for d in `ls -d $ZYNTHIAN_PLUGINS_DIR/lv2/*`; do
-	B=`basename "${d}"`
-	ls -d $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins-fixed/"${B}"/modgui >/dev/null 2>&1
-	if [ $? = 0 ]; then
-			rsync -avP $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins-fixed/"${B}"/ $ZYNTHIAN_PLUGINS_DIR/lv2/"${B}"
-			continue
-	fi
-	ls -d $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins/"${B}"/modgui >/dev/null 2>&1
-	if [ $? = 0 ]; then
-			rsync -avP $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins/"${B}"/ $ZYNTHIAN_PLUGINS_DIR/lv2/"${B}"
-			continue
-	fi
-	ls -d "${d}"/modgui >/dev/null 2>&1
-	if [ $? != 0 ]; then
-		echo "No modgui found for ${B}"
-	fi
-done
-
-#Apply mod-lv2-data
-for d in `ls -d $ZYNTHIAN_PLUGINS_DIR/lv2/*`; do
-	B=`basename "${d}"`
-	ls -d $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins-fixed/"${B}"/modgui >/dev/null 2>&1
-	if [ $? = 0 ]; then
-			rsync -avP $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins-fixed/"${B}"/ /usr/lib/lv2/"${B}"
-			continue
-	fi
-	ls -d $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins/"${B}"/modgui >/dev/null 2>&1
-	if [ $? = 0 ]; then
-			rsync -avP $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins/"${B}"/ /usr/lib/lv2/"${B}"
-			continue
-	fi
-	ls -d "${d}"/modgui >/dev/null 2>&1
-	if [ $? != 0 ]; then
-		echo "No modgui found for ${B}"
-	fi
-done
-
-#Apply mod-lv2-data
-for d in `ls -d $ZYNTHIAN_PLUGINS_DIR/lv2/*`; do
-	B=`basename "${d}"`
-	ls -d $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins-fixed/"${B}"/modgui >/dev/null 2>&1
-	if [ $? = 0 ]; then
-			rsync -avP $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins-fixed/"${B}"/ /usr/local/lib/lv2/"${B}"
-			continue
-	fi
-	ls -d $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins/"${B}"/modgui >/dev/null 2>&1
-	if [ $? = 0 ]; then
-			rsync -avP $ZYNTHIAN_PLUGINS_SRC_DIR/mod-lv2-data/plugins/"${B}"/ /usr/local/lib/lv2/"${B}"
-			continue
-	fi
-	ls -d "${d}"/modgui >/dev/null 2>&1
-	if [ $? != 0 ]; then
-		echo "No modgui found for ${B}"
-	fi
-done
+cd /zynthian
+git clone https://github.com/rodamaralgeo/mod-lv2-data.git
+cd mod-lv2-data
+git pull
+# RakarRack
+cp -R /zynthian/mod-lv2-data/plugins-fixed/rkr.lv2/* /zynthian/zynthian-plugins/lv2/rkr.lv2
+# Fomp Update
+cp -R /zynthian/mod-lv2-data/plugins-fixed/fomp.lv2/* /zynthian/zynthian-plugins/lv2/fomp.lv2
+# Guitarix Updade
+cp -R /zynthian/mod-lv2-data/plugins-fixed/gx_gcb_95.lv2/* /zynthian/zynthian-plugins/lv2/gx_gcb_95.lv2
+cp -R /zynthian/mod-lv2-data/plugins/gxmetal_amp.lv2/* /zynthian/zynthian-plugins/lv2/gxmetal_amp.lv2
+cp -R /zynthian/mod-lv2-data/plugins/gxmetal_head.lv2/* /zynthian/zynthian-plugins/lv2/gxmetal_head.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/gx_mole.lv2/* /zynthian/zynthian-plugins/lv2/gx_mole.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/gx_muff.lv2/* /zynthian/zynthian-plugins/lv2/gx_muff.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/gxts9.lv2/* /zynthian/zynthian-plugins/lv2/gxts9.lv2
+cp -R /zynthian/mod-lv2-data/plugins/gx_colwah.lv2/* /zynthian/zynthian-plugins/lv2/gx_colwah.lv2
+cp -R /zynthian/mod-lv2-data/plugins/gx_zita_rev1.lv2/* /zynthian/zynthian-plugins/lv2/gx_zita_rev1.lv2
+#Zam Audio Plugins
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZaMaximX2.lv2/* /zynthian/zynthian-plugins/lv2/ZaMaximX2.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZaMultiComp.lv2/* /zynthian/zynthian-plugins/lv2/ZaMultiComp.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZaMultiCompX2.lv2/* /zynthian/zynthian-plugins/lv2/ZaMultiCompX2.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamAutoSat.lv2/* /zynthian/zynthian-plugins/lv2/ZamAutoSat.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamComp.lv2/* /zynthian/zynthian-plugins/lv2/ZamComp.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamCompX2.lv2/* /zynthian/zynthian-plugins/lv2/ZamCompX2.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamDelay.lv2/* /zynthian/zynthian-plugins/lv2/ZamDelay.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamEQ2.lv2/* /zynthian/zynthian-plugins/lv2/ZamEQ2.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamGEQ31.lv2/* /zynthian/zynthian-plugins/lv2/ZamGEQ31.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamGate.lv2/* /zynthian/zynthian-plugins/lv2/ZamGate.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamGateX2.lv2/* /zynthian/zynthian-plugins/lv2/ZamGateX2.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamHeadX2.lv2/* /zynthian/zynthian-plugins/lv2/ZamHeadX2.lv2
+cp -R /zynthian/mod-lv2-data/plugins-fixed/ZamTube.lv2/* /zynthian/zynthian-plugins/lv2/ZamTube.lv2
