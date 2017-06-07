2e01bdd7f8627716257d057e8373c494f32c41d8
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-06-06 22:02:10 -0300

    Update install_shiro.sh

--- install_shiro.sh
+++ install_shiro.sh
@@ -1,11 +1,12 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 https://github.com/ninodewit/SHIRO-Plugins.git
-cd
-SHIRO-Plugins
+cd SHIRO-Plugins
 sed -i -- 's,$(DESTDIR)$(PREFIX)/lib/,/zynthian/zynthian-plugins/,g' Makefile
+sed -i -- 's,-mtune=generic -msse -msse2,,g' Makefile.mk
+sed -i -- 's,-mfpmath=sse,,g' Makefile.mk
 git submodule init
 git submodule update
+sed -i -- 's,-mtune=generic -msse -msse2,,g' Makefile.mk dpf/dgl/Makefile.mk
+sed -i -- 's,-mfpmath=sse,,g' Makefile.mk dpf/dgl/Makefile.mk
 make
 make install
-make clean
-cd ..
