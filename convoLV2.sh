59e8c9a573512b03687f659e5b5da8a9b5b69395
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-09-30 17:12:16 -0300

    Update convoLV2.sh

--- convoLV2.sh
+++ convoLV2.sh
@@ -1,18 +1,9 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR 
 git clone https://github.com/x42/convoLV2
 cd convoLV2
-
-sed -i -- 's,PREFIX ?= /usr/local,PREFIX ?= /zynthian,g' Makefile
-sed -i -- 's,LIBDIR ?= lib,LIBDIR ?= zynthian-plugins,g' Makefile
-
-cd /usr/local/lib/lv2
 sed -i -- 's/-msse -msse2 -mfpmath=sse//' Makefile
 sed -i -- 's/LV2DIR ?= \$(PREFIX)\/lib\/lv2/LV2DIR ?= \/zynthian\/zynthian-plugins\/lv2/' Makefile
-make submodules
-make -j 4 MOD=1
-sudo make MOD=1 install
+make -j 4
+sudo make install INSTDIR=/zynthian/zynthian-plugins/ir.lv2
 make clean
 cd ..
-
-
-INSTDIR=/zynthian/zynthian-plugins/ir.lv2
