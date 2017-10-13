22c3add41bf979b916553ea038ef904e9e71f682
rodamaralgeo <rodrigo.geoflex@gmail.com>
2017-10-13 12:03:53 -0300

    Update abgate.sh

--- abgate.sh
+++ abgate.sh
@@ -2,7 +2,8 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/antanasbruzas/abGate.git
 cd abGate
-apt-get install gtkmm-2.4
+suod apt-get install -y gtkmm-2.4
+sudo apt-get -y install qt4-dev-tools
 make
 make install INSTALL_DIR=/zynthian/zynthian-plugins/lv2
 make clean
