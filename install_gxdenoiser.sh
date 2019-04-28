375be175a1e5a074d5b544ea7ec9d23be26508d0
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-04-28 19:04:40 -0300

    Update install_gxdenoiser.sh

--- install_gxdenoiser.sh
+++ install_gxdenoiser.sh
@@ -2,6 +2,6 @@ cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/brummer10/GxDenoiser2.lv2.git
 cd GxDenoiser2.lv2
 make 
-make install mod INSTALL_DIR=/zynthian/zynthian-plugins/lv2
+make install INSTALL_DIR=/zynthian/zynthian-plugins/lv2
 make clean
 cd ..
