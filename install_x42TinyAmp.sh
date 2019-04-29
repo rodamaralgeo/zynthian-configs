4bed95c4b7cabf6127247466a8f0fa69de9c7e7d
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-04-29 14:28:26 -0300

    Create install_x42TinyAmp.sh

---
+++ install_x42TinyAmp.sh
@@ -0,0 +1,8 @@
+cd $ZYNTHIAN_PLUGINS_SRC_DIR
+git clone https://github.com/x42/tuna.lv2.githttps://github.com/x42/tinyamp.lv2.git
+cd tinyamp.lv2
+sed -i -- 's,PREFIX ?= /usr/local,PREFIX ?= /zynthian/zynthian-plugins,g' Makefile
+sed -i -- 's,LV2DIR ?= $(PREFIX)/lib/lv2,LV2DIR ?= $(PREFIX)/lv2,g' Makefile 
+sed -i -- 's,-msse -msse2 -mfpmath=sse,,g' Makefile 
+make MOD=1
+make install MOD=1
