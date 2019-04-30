da81be10881b6a2c0aca8389e5b9ee7a9fb95a05
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-04-29 21:54:23 -0300

    Update install_bollie.sh

--- install_bollie.sh
+++ install_bollie.sh
@@ -1,6 +1,14 @@
 #bollie plugins
 
 cd $ZYNTHIAN_SW_DIR/plugins
+git clone https://github.com/MrBollie/bolliedelay.lv2.git
+cd bolliedelay.lv2
+sed -i -- "s/lib\/lv2//" Makefile
+sudo make PREFIX="" DESTDIR="$ZYNTHIAN_PLUGINS_DIR/lv2" install
+make clean
+cd ..
+
+cd $ZYNTHIAN_SW_DIR/plugins
 git clone https://github.com/MrBollie/bolliedelayxt.lv2.git
 cd bolliedelayxt.lv2
 sed -i -- "s/lib\/lv2//" Makefile
