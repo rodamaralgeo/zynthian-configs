2940e8012926fabf611206a1dd6f8be3c3f7f1dc
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-05-01 18:38:03 -0300

    Update install_x42TinyAmp.sh

--- install_x42TinyAmp.sh
+++ install_x42TinyAmp.sh
@@ -1,5 +1,5 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
-git clone https://github.com/x42/tuna.lv2.githttps://github.com/x42/tinyamp.lv2.git
+git clone https://github.com/x42/tinyamp.lv2.git
 cd tinyamp.lv2
 sed -i -- 's,PREFIX ?= /usr/local,PREFIX ?= /zynthian/zynthian-plugins,g' Makefile
 sed -i -- 's,LV2DIR ?= $(PREFIX)/lib/lv2,LV2DIR ?= $(PREFIX)/lv2,g' Makefile 
