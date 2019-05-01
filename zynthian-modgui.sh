475d11e85d4e0c5c9e473cb75937ac9fb352e229
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-05-01 13:38:32 -0300

    Update zynthian-modgui.sh

--- zynthian-modgui.sh
+++ zynthian-modgui.sh
@@ -1,19 +1,5 @@
 # zynthian-modgui
-. $ZYNTHIAN_DIR/zynthian-recipe/recipe/_zynth_lib.sh
 cd $ZYNTHIAN_DIR
-zynth_git https://github.com/zynthian/zynthian-modgui.git
-if [ ${?} -ne 0 -o  "${build}" = "build" ]
-then
-	zynth_build_request clear
-	cd zynthian-modgui
-	mkdir -p $ZYNTHIAN_PLUGINS_DIR/lv2
-	for i in *.modgui
-	do
-		if [ -L $ZYNTHIAN_PLUGINS_DIR/lv2/$i ]
-		then
-			rm $ZYNTHIAN_PLUGINS_DIR/lv2/$i
-		fi
-		ln -s $ZYNTHIAN_DIR/zynthian-modgui/$i $ZYNTHIAN_PLUGINS_DIR/lv2
-	done
-	zynth_build_request ready
-fi
+gitclone https://github.com/zynthian/zynthian-modgui.git
+cd zynthian-modgui
+ln -s $ZYNTHIAN_DIR/zynthian-modgui/*.modgui $ZYNTHIAN_PLUGINS_DIR/lv2
