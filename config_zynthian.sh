471766e34f34e386e4e0336bbccf3eb47c9cc6c1
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-05-01 17:03:12 -0300

    Update config_zynthian.sh

--- config_zynthian.sh
+++ config_zynthian.sh
@@ -1,6 +1,6 @@
 sh	install_GxCreamMachine.sh
 sh	install_abgate.sh
-sh	install_alo_looper.sh
+#sh	install_alo_looper.sh
 sh	install_bollie.sh
 sh	install_cabsim.sh
 sh	install_convoLV2.sh
@@ -12,7 +12,7 @@ sh	install_gxplexi.sh
 sh	install_invada.sh
 sh	install_irlv2.sh
 sh	install_jackPiledmeter.sh
-sh	install_midding.sh
+#sh	install_midding.sh
 sh	install_mod-meter.sh
 sh	install_mod-pitchshifter.sh
 sh	install_rkrlv2.sh
@@ -21,5 +21,6 @@ sh	install_shiro.sh
 sh	install_tap.sh
 sh	install_x42_eq.sh
 sh	install_x42tuna.sh
+sh install_x42TinyAmp.sh
 sh	install_zamaudio.sh
-sh zynthian-modgui.sh
+#sh  zynthian-modgui.sh
