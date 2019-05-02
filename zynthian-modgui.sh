a21198f8df47f9fc0335beef69470c5c6186fcf3
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-05-01 21:50:23 -0300

    Update zynthian-modgui.sh

--- zynthian-modgui.sh
+++ zynthian-modgui.sh
@@ -1,5 +1,5 @@
 # zynthian-modgui
 cd $ZYNTHIAN_DIR
-gitclone https://github.com/zynthian/zynthian-modgui.git
+gitclone https://github.com/rodamaralgeo/zynthian-modgui.git
 cd zynthian-modgui
 ln -s $ZYNTHIAN_DIR/zynthian-modgui/*.modgui $ZYNTHIAN_PLUGINS_DIR/lv2
