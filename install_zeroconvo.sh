f150f4cbead8fbc06f2cfbd9c3d9ae8a8633bfce
rodamaralgeo <rodrigo.geoflex@gmail.com>
2019-06-21 12:17:38 -0300

    Update install_zeroconvo.sh

--- install_zeroconvo.sh
+++ install_zeroconvo.sh
@@ -1,4 +1,5 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR 
+rm -rf zeroconvo.lv2
 git clone git://gareus.org/zeroconvo.lv2
 cd zeroconvo.lv2
 sed -i -- 's/-msse -msse2 -mfpmath=sse//' Makefile
@@ -7,9 +8,15 @@ sed -i -- 's/LV2DIR ?= \$(PREFIX)\/lib\/lv2/LV2DIR ?= \/zynthian\/zynthian-plugi
 
 wget https://x42-plugins.com/tmp/sisel4-ir.tar.xz
 tar xf sisel4-ir.tar.xz
+mkdir build
+mkdir build/ir
+mv sisel4-ir.lv2/*.wav build/ir/ 
 cat sisel4-ir.lv2/manifest.ttl | tail -n +7 >> build/manifest.ttl
 cat sisel4-ir.lv2/presets.ttl | tail -n +10 >> build/presets.ttl
+
+
 make 
 make install
+sed -e "s|#ir> <|#ir> <ir/|" -i /zynthian/zynthian-plugins/lv2/zeroconvo.lv2/presets.ttl
 make clean
 cd ..
