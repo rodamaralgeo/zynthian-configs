c06d9d82d46639897573db414d28c5a20f653552
rodamaralgeo <rodrigo.geoflex@gmail.com>
2020-05-30 16:39:40 -0300

    Update install_gula.sh

--- install_gula.sh
+++ install_gula.sh
@@ -2,5 +2,6 @@ cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/steveb/gula-plugins.git
 cd gula-plugins
 make all
+cp -R $ZYNTHIAN_PLUGINS_SRC_DIR/gula-plugins/lv2/*.lv2 /zynthian/zynthian-plugins/lv2
 make install
 make clean
