d86f0616292c6f09a6c26930bed71eceb1ca20ac
rodamaralgeo <rodrigo.geoflex@gmail.com>
2020-05-31 08:06:12 -0300

    Create install_Kapitonov.sh

---
+++ install_Kapitonov.sh
@@ -0,0 +1,14 @@
+https://github.com/olegkapitonov/Kapitonov-Plugins-Pack.git
+
+
+cd $ZYNTHIAN_PLUGINS_SRC_DIR
+rm -rf $ZYNTHIAN_PLUGINS_SRC_DIR/Kapitonov-Plugins-Pack
+git clone --single-branch --branch thirdparty-included https://github.com/olegkapitonov/Kapitonov-Plugins-Pack.git
+cd Kapitonov-Plugins-Pack
+sed -i -- 's,'gui': true,'gui': false,g' meson.build
+sed -i -- 's,'ladspa': true,'ladspa': false,g' meson.build
+meson build --reconfigure -Dlv2dir=/zynthian/zynthian-plugins/lv2 --prefix $HOME and then ninja -C build install.
+make mod
+make install mod INSTALL_DIR=/zynthian/zynthian-plugins/lv2
+make clean
+cd ..
